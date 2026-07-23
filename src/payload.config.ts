d5e2fb1189167a693c0dce7508a642a481a9b4ba
shahadat-hossen008 <penaxavier078@gmail.com>
2026-07-23 12:00:30 +0600

    feat: add Tags collection to manage tag data and update payload configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/media'
+import { Tags } from './collections/Tags'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -18,7 +19,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, Tags],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
